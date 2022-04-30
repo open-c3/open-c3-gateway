3c76ae743aeeecc569d22ff6f024fd7f8c707921
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-04-30 23:57:34 +0800

    fix md5.lua

--- conf/lua/md5.lua
+++ conf/lua/md5.lua
@@ -1,6 +1,6 @@
 if ngx.var.cookie_session then
     local md5 = ngx.md5(ngx.var.cookie_session)
-    if not ngx.var.cookie_session == "cookietimeout" then
+    if not ( ngx.var.cookie_session == "cookietimeout" ) then
         ngx.log(ngx.ERR, "cookieinfo:[", os.time(), ":",md5,"]")
     end
     return md5
